0f36e8ac37ed33ce85702287926e4d4a2a131893
MikeyMoore <icarusjedi@gmail.com>
2016-12-05 21:34:24 -0800

    Add js file, change color with menu hover

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.menu a').hover(
+    function(){
+      $(this).addClass('active');
+    },
+    function(){
+      $(this).removeClass('active');
+    }
+  );
+});
